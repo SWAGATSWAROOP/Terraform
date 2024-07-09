9bf034b6c498cc17665af85cb18f7db8d3bfe60d
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-07-09 09:01:46 +0530

    Starting Terraform

---
+++ main.tf
@@ -0,0 +1,9 @@
+#1. Create your VPC
+#2. Create a public subnet
+#3. Create a private subnet
+#4. Create a internet gateway
+#5. Create a route table's
+#6. Create a route table association with public subneterraform
+
+
+
